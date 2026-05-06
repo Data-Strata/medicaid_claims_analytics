c2ece4bd1f5ff3ed09f70c25fd19ded1123ef988
Mairilyn Yera Galindo <mairilynyera@gmail.com>
2026-05-06 14:28:41 -0400

    Update medicaid_full_pipeline.sql

--- sql/medicaid_full_pipeline.sql
+++ sql/medicaid_full_pipeline.sql
@@ -4,7 +4,17 @@
 -- only works if you use SnowSQL !source commands.
 -- ============================================================
 
+-- RAW
 !source medicaid_ingestion_raw.sql;
+
+-- CLEAN
 !source medicaid_clean_stage.sql;
+
+-- Creates DATE_DIM and SERVICE_CATEGORY_DIM for Power BI modeling  
+!source date_and_service_dimensions.sql 
+
+-- CREATE FACT TABLE THAT CONNECTS TO MODEL DIMENSION TABLES 
 !source medicaid_fact_table.sql;
+
+-- VALIDATE
 !source medicaid_quality_checks.sql;
